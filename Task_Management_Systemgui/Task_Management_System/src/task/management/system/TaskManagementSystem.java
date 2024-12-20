458c14b9f649764e04d8005fdc2b51b9e759d2fb
Nourhan Mohamed <110159479+NourhanElmeshneb@users.noreply.github.com>
2024-12-20 23:35:23 +0200

    Add files via upload

---
+++ Task_Management_Systemgui/Task_Management_System/src/task/management/system/TaskManagementSystem.java
@@ -0,0 +1,17 @@
+
+package task.management.system;
+
+/**
+ *
+ * @author 20121
+ */
+public class TaskManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
